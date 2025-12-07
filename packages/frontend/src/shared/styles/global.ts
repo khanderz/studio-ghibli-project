e02392ba166bd5a9a13fcb993b7ff5576acb87c8
Khanh <82117702+khanderz@users.noreply.github.com>
2025-12-07 13:35:28 -0600

    style: background, layout, global typography

--- packages/frontend/src/shared/styles/global.ts
+++ packages/frontend/src/shared/styles/global.ts
@@ -2,14 +2,21 @@ import { css } from '@mui/material';
 import { theme } from '~/shared/styles/theme';
 
 export const globalStyles = css`
-  body,
   html,
+  body,
   #root {
     height: 100%;
     display: flex;
     margin: 0;
     padding: 0;
-    background-color: ${theme.palette.common.white};
-    font-family: sans-serif;
+  }
+
+  body {
+    background-image: url('/Background.png');
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
+    font-family: ${theme.typography.fontFamily};
+    color: ${theme.palette.text.primary || '#000'};
   }
 `;
--- packages/frontend/src/shared/styles/theme/typography.ts
+++ packages/frontend/src/shared/styles/theme/typography.ts
@@ -1,7 +1,30 @@
-export const typography = {
-  h1: {},
-  h2: {},
-  h3: {},
-  body1: {},
-  body2: {},
+import { TypographyOptions } from '@mui/material/styles/createTypography';
+
+export const typography: TypographyOptions = {
+  fontFamily:
+    '"Montserrat", -apple-system, BlinkMacSystemFont, "Segoe UI", system-ui, sans-serif',
+
+  h1: {
+    fontWeight: 600,
+    fontSize: '60px',
+    lineHeight: '20px',
+  },
+  h2: {
+    fontWeight: 400,
+    fontSize: '30px',
+    lineHeight: '20px',
+  },
+  h3: {
+    fontWeight: 800,
+    fontSize: '20px',
+    lineHeight: '20px',
+  },
+  body1: {
+    fontSize: '16px',
+    lineHeight: 1.5,
+  },
+  body2: {
+    fontSize: '14px',
+    lineHeight: 1.5,
+  },
 };
